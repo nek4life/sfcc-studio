3173cafea4b141607da9cc4cb7c00425976705bd
Charlie Choiniere <nek4life@gmail.com>
2019-01-30 18:56:27 -0500

    Fixed spelling mistake

--- src/main/java/com/binarysushi/studio/StudioIcons.java
+++ src/main/java/com/binarysushi/studio/StudioIcons.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 
 public class StudioIcons {
     public static final Icon STUDIO_ICON = IconLoader.getIcon("/icons/salesforce.png");
-    public static final Icon STUDIO_CARTIDGE_ICON = IconLoader.getIcon("/icons/cartridge.png");
+    public static final Icon STUDIO_CARTRIDGE_ICON = IconLoader.getIcon("/icons/cartridge.png");
     public static final Icon STUDIO_ISML_ICON = IconLoader.getIcon("/icons/isml-file-icon.png");
     public static final Icon STUDIO_DS_ICON = IconLoader.getIcon("/icons/ds-file-icon.png");
 }
