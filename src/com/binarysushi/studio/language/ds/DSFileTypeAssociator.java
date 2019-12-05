72a23116d9450153dffa2108d513d1870e06152b
Charlie Choiniere <nek4life@gmail.com>
2019-12-05 12:30:11 -0500

    Removed deprecated application component API so plugins can be loaded without restarting the IDE in 2020.1

--- src/com/binarysushi/studio/language/ds/DSFileTypeAssociator.java
+++ src/com/binarysushi/studio/language/ds/DSFileTypeAssociator.java
@@ -1,28 +1,20 @@
 package com.binarysushi.studio.language.ds;
 
-import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.fileTypes.FileTypeManager;
 import com.intellij.openapi.fileTypes.UnknownFileType;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupActivity;
 import org.jetbrains.annotations.NotNull;
 
-public class DSFileTypeAssociator implements ApplicationComponent {
+public class DSFileTypeAssociator implements StartupActivity {
     FileTypeManager fileTypeManager = FileTypeManager.getInstance();
 
     @Override
-    public void initComponent() {
+    public void runActivity(@NotNull Project project) {
         FileType javaScriptFileType = fileTypeManager.getFileTypeByExtension("js");
         if (!(javaScriptFileType instanceof UnknownFileType)) {
             fileTypeManager.associateExtension(javaScriptFileType, "ds");
         }
-    }
-
-    @Override
-    public void disposeComponent() {}
-
-    @NotNull
-    @Override
-    public String getComponentName() {
-        return "DSFileTypeAssociator";
     }
 }
