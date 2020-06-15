0759338edf535532bd0110ea5b239866b3687f16
Charlie Choiniere <nek4life@gmail.com>
2020-06-15 11:59:24 -0400

    Fixed compilation error

--- src/com/binarysushi/studio/language/isml/codeInsight/ISMLLineMarkerProvider.java
+++ src/com/binarysushi/studio/language/isml/codeInsight/ISMLLineMarkerProvider.java
@@ -20,7 +20,7 @@ import java.util.Collection;
 public class ISMLLineMarkerProvider extends RelatedItemLineMarkerProvider {
 
     @Override
-    protected void collectNavigationMarkers(@NotNull PsiElement element, @NotNull Collection<? super RelatedItemLineMarkerInfo> result) {
+    protected void collectNavigationMarkers(@NotNull PsiElement element, @NotNull Collection<? super RelatedItemLineMarkerInfo<?>> result) {
         if (!(element instanceof XmlAttribute)) {
             return;
         }
