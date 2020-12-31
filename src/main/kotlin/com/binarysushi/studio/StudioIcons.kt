56849cce2647efef1f274a8db40bd9f46cae5932
Charlie Choiniere <nek4life@gmail.com>
2020-12-31 08:27:09 -0500

    Fixed deprecation warning

--- src/main/kotlin/com/binarysushi/studio/StudioIcons.kt
+++ src/main/kotlin/com/binarysushi/studio/StudioIcons.kt
@@ -4,14 +4,14 @@ import com.intellij.openapi.util.IconLoader
 
 object StudioIcons {
     @JvmField
-    val STUDIO_ICON = IconLoader.getIcon("/icons/salesforce.png")
+    val STUDIO_ICON = IconLoader.getIcon("/icons/salesforce.png", StudioIcons::class.java)
 
     @JvmField
-    val STUDIO_CARTRIDGE_ICON = IconLoader.getIcon("/icons/cartridge.png")
+    val STUDIO_CARTRIDGE_ICON = IconLoader.getIcon("/icons/cartridge.png", StudioIcons::class.java)
 
     @JvmField
-    val STUDIO_ISML_ICON = IconLoader.getIcon("/icons/isml-file-icon.png")
+    val STUDIO_ISML_ICON = IconLoader.getIcon("/icons/isml-file-icon.png", StudioIcons::class.java)
 
     @JvmField
-    val STUDIO_DS_ICON = IconLoader.getIcon("/icons/ds-file-icon.png")
+    val STUDIO_DS_ICON = IconLoader.getIcon("/icons/ds-file-icon.png", StudioIcons::class.java)
 }
