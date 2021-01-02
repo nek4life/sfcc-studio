f39a3b307756c8a5249a2c781644cdd71a14ffbd
Charlie Choiniere <nek4life@gmail.com>
2021-01-02 09:21:09 -0500

    Added some basic file filtering and results

--- src/main/kotlin/com/binarysushi/studio/language/javascript/RequireCompletionProvider.kt
+++ src/main/kotlin/com/binarysushi/studio/language/javascript/RequireCompletionProvider.kt
@@ -2,7 +2,13 @@ package com.binarysushi.studio.language.javascript
 
 import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.*
+import com.intellij.json.*
+import com.intellij.lang.javascript.*
+import com.intellij.openapi.project.*
+import com.intellij.openapi.vfs.*
+import com.intellij.psi.search.*
 import com.intellij.util.*
+import org.jetbrains.annotations.*
 
 class RequireCompletionProvider : CompletionProvider<CompletionParameters>() {
     override fun addCompletions(
@@ -11,7 +17,7 @@ class RequireCompletionProvider : CompletionProvider<CompletionParameters>() {
         result: CompletionResultSet
     ) {
         val caretPositionOffset = parameters.offset - parameters.position.textOffset
-        val query = parameters.position.text.substring(1, caretPositionOffset)
+        val query = parameters.position.text.substring(1, caretPositionOffset) // Drop the first quote
 
         when {
             query.startsWith("*") -> handleStarCompletion(parameters, context, result)
@@ -23,16 +29,34 @@ class RequireCompletionProvider : CompletionProvider<CompletionParameters>() {
         }
     }
 
+    private fun findFiles(project: @NotNull Project): List<VirtualFile> {
+        val files = FileTypeIndex.getFiles(
+            JavaScriptFileType.INSTANCE,
+            GlobalSearchScope.projectScope(project)
+        ) + FileTypeIndex.getFiles(JsonFileType.INSTANCE, GlobalSearchScope.projectScope(project))
+
+        return files.filter { it.path.contains("cartridge") || it.path.contains("module") }
+    }
+
+    /**
+     * Tilde should only complete files that are relative to the cartridge path where the original
+     * file is located
+     *
+     */
     private fun handleTildeCompletion(
         parameters: CompletionParameters,
         context: ProcessingContext,
         result: CompletionResultSet
     ) {
-        val filePath = parameters.originalFile.virtualFile.path
-
-        result.addElement(LookupElementBuilder.create("tilde"))
+        findFiles(parameters.position.project).forEach {
+            result.addElement(LookupElementBuilder.create(it))
+        }
     }
 
+    /**
+     * Star should complete files in any cartridge on the active cartridge path
+     *
+     */
     private fun handleStarCompletion(
         parameters: CompletionParameters,
         context: ProcessingContext,
